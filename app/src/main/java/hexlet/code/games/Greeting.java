77ea23ad189dc4332f61bec9f104535399ca8335
apzakharov <apzakharov9881@gmail.com>
2025-12-10 15:31:36 +0300

    nine step

--- app/src/main/java/hexlet/code/games/Greeting.java
+++ app/src/main/java/hexlet/code/games/Greeting.java
@@ -1,9 +1,18 @@
 package hexlet.code.games;
 
+import hexlet.code.engine.Player;
 import hexlet.code.games.quest.GreetingQuest;
 import hexlet.code.games.quest.Quest;
 
+import static hexlet.code.engine.EngineContext.currentPlayer;
+
 public class Greeting extends SimpleGame {
+
+    @Override
+    public String congratulations(String answer) {
+        currentPlayer.set(new Player(answer));
+        return "Hellow, %s!".formatted(answer);
+    }
 
     public Greeting() {
         super(new GreetingQuest());
--- app/src/main/java/hexlet/code/games/quest/GreetingQuest.java
+++ app/src/main/java/hexlet/code/games/quest/GreetingQuest.java
@@ -1,9 +1,5 @@
 package hexlet.code.games.quest;
 
-import hexlet.code.engine.Player;
-
-import static hexlet.code.engine.EngineContext.currentPlayer;
-
 public class GreetingQuest extends Quest {
     private static String welcome = "Welcome to the Brain Games!";
     private static String askName = "May I have your name?";
@@ -12,9 +8,4 @@ public class GreetingQuest extends Quest {
         super(String.join("\n", welcome, askName), "", (string) -> true);
     }
 
-    @Override
-    public String answer() {
-        Player player = currentPlayer.get();
-        return "Hellow, %s!".formatted(player.name());
-    }
 }
